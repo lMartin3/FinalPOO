dcce4755a68687bb353ca56ba8bafbdf76128431
lMartin3 <me@martinl.dev>
2023-08-06 20:27:04 -0300

    build: agregado de "Util" para clases útiles, y de CondicionFiscalUtil para ayudar a vincular strings con condiciones fiscales

---
+++ Modelo/Util/CondicionFiscalUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Util
+{
+    public class CondicionFiscalUtil
+    {
+        private static readonly Dictionary<String, CondicionFiscal> condiciones = new Dictionary<String, CondicionFiscal>() {
+            { "CONSUMIDOR FINAL", CondicionFiscal.CONSUMIDOR_FINAL },
+            { "MONOTRIBUTISTA", CondicionFiscal.MONOTRIBUTISTA },
+            { "RESPONSABLE INSCRIPTO", CondicionFiscal.RESPONSABLE_INSCRIPTO },
+        };
+
+        public static String[] CondicionesComoStrings()
+        {
+            
+                return condiciones.Keys.ToArray();
+            
+        }
+
+        public static CondicionFiscal? CondicionPorString(String str)
+        {
+            if(condiciones.ContainsKey(str))
+            {
+                return condiciones[str];
+            }
+            return null;
+        }
+    }
+}
