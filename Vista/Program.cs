1a31bd5ee2f8792934909f9bc9dd955c98c5403b
lMartin3 <me@martinl.dev>
2023-12-10 17:55:25 -0300

    refactor: cambiar el proyecto de la vista de .NET Framework a .NET 6.0 LTS

---
+++ Vista/Program.cs
@@ -0,0 +1,21 @@
+using Modelo;
+using PapeleriaGUI;
+
+namespace Vista
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Papeleria papeleria = new Papeleria();
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form2(papeleria));
+        }
+    }
+}
