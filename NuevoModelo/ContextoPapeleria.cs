27dc3a86f58be378789aba344f00be139110993c
lMartin3 <me@martinl.dev>
2023-12-10 20:42:25 -0300

    build: Primera parte para el ContextoPapeleria

---
+++ NuevoModelo/ContextoPapeleria.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NuevoModelo.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuevoModelo
+{
+    public class ContextoPapeleria : DbContext
+    {
+        private static ContextoPapeleria instancia;
+
+        public static ContextoPapeleria Instancia {
+            get { 
+                if(instancia == null)
+                {
+                    instancia = new ContextoPapeleria();
+                }
+                return instancia;
+            } 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            string connectionString;
+            IConfigurationRoot configuration = ConfigurationHelper.GetConfiguration("appSettings.json");
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+            options.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ NuevoModelo/Util/ConfigurationHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuevoModelo.Util
+{
+    public static class ConfigurationHelper
+    {
+        public static IConfigurationRoot GetConfiguration(string fileName)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(fileName, optional: true, reloadOnChange: true);
+            return builder.Build();
+        }
+    }
+}
