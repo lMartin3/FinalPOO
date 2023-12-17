b6a2a196127bf6775351805422f1165951d8153a
lMartin3 <me@martinl.dev>
2023-12-17 15:07:41 -0300

    fix: Setters y Getters para los DBSet

--- NuevoModelo/ContextoPapeleria.cs
+++ NuevoModelo/ContextoPapeleria.cs
@@ -24,15 +24,21 @@ namespace NuevoModelo
             } 
         }
 
-        public DbSet<CategoriaProducto> Categorias;
-        public DbSet<Producto> Productos;
-        public DbSet<Proveedor> Proveedores;
-        public DbSet<OrdenCompra> OrdenesDeCompra;
-        public DbSet<Cliente> Clientes;
-        public DbSet<Venta> Ventas;
+        public ContextoPapeleria() : base()
+        {
+
+        }
+
+        public DbSet<CategoriaProducto> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }  
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<OrdenCompra> OrdenesDeCompra { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
+            base.OnConfiguring(options);
             string connectionString;
             IConfigurationRoot configuration = ConfigurationHelper.GetConfiguration("appSettings.json");
             connectionString = configuration.GetConnectionString("DefaultConnection");
@@ -41,6 +47,7 @@ namespace NuevoModelo
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             // Registrar restricción (constraint) de DB de DNI / CUIT único
             modelBuilder.Entity<Cliente>().HasIndex(c => c.DniOCuit).IsUnique();
         }
